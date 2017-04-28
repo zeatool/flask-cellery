b79da7e9fb937a09cae474654808e4780ab8c560
Baranov-rp <baranov-rp@biryusa.ru>
2017-04-28 11:52:47 +0700

    Переделал рассчет на класс, чтобы удобнее тестировался код

--- flask_celery.py
+++ flask_celery.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from celery import Celery
+from itertools import cycle
 
 UPLOAD_FOLDER = r'C:/pyProjects/flask_celery/tmp'
 
@@ -18,7 +19,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-
 celery = Celery(app.name)
 celery.conf.update(app.config)
 
@@ -28,13 +28,72 @@ app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
+
 class GraphRecord(db.Model):
-    id = db.Column('id', db.Integer, primary_key = True)
+    id = db.Column('id', db.Integer, primary_key=True)
     task_id = db.Column(db.String)
     weight = db.Column(db.Integer)
     state = db.Column(db.String)
     count = db.Column(db.Integer)
-    date_add = db.Column(db.DateTime,default=datetime.datetime.now)
+    date_add = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+class GraphCalculator():
+    filename = ''
+    graph = {}
+    weight = 0
+    count = 0
+
+    def __init__(self, filename=None):
+        if filename != None:
+            self.filename = filename
+            tree = ElementTree.parse(os.path.join(app.config['UPLOAD_FOLDER'], self.filename)).getroot()
+            self.elems = iter(tree.findall('item'))
+
+    def next(self):
+        try:
+            elem = next(self.elems)
+        except StopIteration:
+            return False
+
+        if (elem.find('id') != None):
+            id = elem.find('id').text
+
+            if (self.graph.get(id) == None):
+                self.graph[id] = {'childs': set(), 'created': True}
+            else:
+                self.graph[id]['created'] = True
+
+            if elem.find('parentId') != None:
+                parent = elem.find('parentId').text
+
+                if (self.graph.get(parent) == None):
+                    self.graph[parent] = {'childs': set(), 'created': False}
+
+                self.graph[parent]['childs'].add(id)
+
+        self.count += 1
+        return True
+
+    def getMeta(self):
+        return {'count': self.count, 'weight': self.weight}
+
+    def calculate_graph_weight(self):
+        self.weight = self.get_graph_weight(self.graph)
+
+        return self.weight
+
+    @staticmethod
+    def get_graph_weight(graph):
+        weight = 0
+
+        for point in graph.values():
+            if (point.get('created') == True):
+                if point.get('childs') and isinstance(point['childs'], set):
+                    weight += len(point['childs'])
+
+        return weight
+
 
 @celery.task(bind=True)
 def test_task(self):
@@ -44,11 +103,33 @@ def test_task(self):
 
     return {'count': 10}
 
+
 @celery.task(bind=True)
-def parseXml(self,filename):
+def calculate_graph_from_file(self, filename):
+    graph = GraphCalculator(filename)
+    meta = {'count': 0, 'weight': 0}
+
+    while graph.next():
+        meta = graph.getMeta()
+        self.update_state(state='PARSE', meta=meta)
+
+    self.update_state(state='CALCULATE', meta=meta)
+    graph.calculate_graph_weight()
+    meta = graph.getMeta()
+    self.update_state(state='CALCULATE', meta=meta)
+
+    GraphRecord.query.filter_by(task_id=self.request.id).update(
+        {'weight': meta['weight'], 'state': 'SUCCESS', 'count': meta['count']})
+    db.session.commit()
+
+    return meta
+
+# depricated
+@celery.task(bind=True)
+def parseXml(self, filename):
     e = ElementTree.parse(os.path.join(app.config['UPLOAD_FOLDER'], filename)).getroot()
     graph = {}
-    meta = {'count':0,'weight':0}
+    meta = {'count': 0, 'weight': 0}
 
     for elem in e.findall('item'):
         time.sleep(0.5)
@@ -65,31 +146,42 @@ def parseXml(self,filename):
                 graph[parent] = {'childs': set(), 'created': False}
 
             graph[parent]['childs'].add(id)
-        meta['count']+=1
-        self.update_state(state='PARSE',meta=meta)
+        meta['count'] += 1
+        self.update_state(state='PARSE', meta=meta)
 
     self.update_state(state='CALCULATE', meta=meta)
-    weight = 0
-    for point in graph.values():
-        weight += len(point['childs'])
+
+    weight = calculate_graph_weight(graph)
     meta['weight'] = weight
 
-    self.update_state(state='CALCULATE', meta=meta)
-    GraphRecord.query.filter_by(task_id=self.request.id).update({'weight':weight,'state':'SUCCESS','count':meta['count']})
+    self.update_state(state='CALCULATED', meta=meta)
+    GraphRecord.query.filter_by(task_id=self.request.id).update(
+        {'weight': weight, 'state': 'SUCCESS', 'count': meta['count']})
     db.session.commit()
 
     return meta
 
 
+def calculate_graph_weight(graph):
+    weight = 0
+
+    for point in graph.values():
+        if (point.get('created') == True):
+            if point.get('childs') and isinstance(point['childs'], set):
+                weight += len(point['childs'])
+
+    return weight
+
+
 @app.route('/')
 def index():
-    #task = parseXml.apply_async(kwargs={'filename': 'test.xml'})
-    #record = GraphRecord(task_id=task.id,weight=0)
+    # task = parseXml.apply_async(kwargs={'filename': 'test.xml'})
+    # record = GraphRecord(task_id=task.id,weight=0)
 
-    #db.session.add(record)
-    #db.session.commit()
+    # db.session.add(record)
+    # db.session.commit()
 
-    return render_template('index.html', tasks=GraphRecord.query.order_by(GraphRecord.date_add.desc()).slice(0,20))
+    return render_template('index.html', tasks=GraphRecord.query.order_by(GraphRecord.date_add.desc()).slice(0, 20))
 
 
 @app.route('/check/<task_id>')
@@ -97,28 +189,34 @@ def check(task_id):
     task = parseXml.AsyncResult(task_id)
 
     result = {
-        'STATE' : task.state,
-        'COUNT' : task.info.get('count'),
-        'WEIGHT': task.info.get('weight'),
+        'STATE': task.state,
+        'COUNT': 0,
+        'WEIGHT': 0,
     }
+
+    if task.info != None:
+        result['COUNT'] = task.info.get('count')
+        result['WEIGHT'] = task.info.get('weight')
+
     return jsonify(result)
 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        salt = str(datetime.datetime.now().timestamp()).replace('.','')
+        salt = str(datetime.datetime.now().timestamp()).replace('.', '')
         file = request.files['tree']
-        filename = salt+'_'+secure_filename(file.filename)
+        filename = salt + '_' + secure_filename(file.filename)
 
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        task = parseXml.apply_async(kwargs={'filename': filename})
-        record = GraphRecord(task_id=task.id,weight=0)
+        task = calculate_graph_from_file.apply_async(kwargs={'filename': filename})
+        record = GraphRecord(task_id=task.id, weight=0)
 
         db.session.add(record)
         db.session.commit()
 
         return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     db.create_all()
--- tests.py
+++ tests.py
@@ -3,21 +3,70 @@ import flask_celery
 import unittest
 import tempfile
 
+
 class FlaskCeleryTestCase(unittest.TestCase):
     def setUp(self):
-        self.db_fd, flask_celery.app.config['DATABASE'] = tempfile.mkstemp()
-        flask_celery.app.config['TESTING'] = True
-        self.app = flask_celery.app.test_client()
-        flask_celery.init_db()
+        b = 1
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flask_celery.app.config['DATABASE'])
+        b = 0
 
     def test_hello(self):
-        assert True==True
+        assert True == True
+
+    def test_graph_weight_calculate(self):
+
+        graph = {
+            '1': {'childs': {'2', '3', '4'},'created':True}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 3
+
+        graph = {
+            '1': {'childs': {'2', '3', '4'},'created':False}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+        graph = {
+            '1': {'childs': {'2', '1', '4'}, 'created': True},
+            '2': {'childs': {'4'}, 'created': False},
+            '5': {'childs': {'3', '8', '9'}, 'created': True}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 6
+
+        graph = {}
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+        graph = {
+            '1': {'childs': {'2', '3', '4'}}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+        graph = {
+            '1': {'chilrrrrrrrrds': {'2', '3', '4'},'everybody':'dance now'}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+        graph = {
+            '1': {'childs': 999,'created':True}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+        graph = {
+            '1': {'childs': {'1'}, 'created': {'454'}}
+        }
+        assert flask_celery.GraphCalculator.get_graph_weight(graph) == 0
+
+    def test_graph_calculate(self):
+        t = flask_celery.GraphCalculator('test.xml')
+        while t.next():
+            t.getMeta()
+
+        print(t.calculate_graph_weight())
+        #flask_celery.parseXml('test.xml')
+        assert True == True
+
+
 
 
 if __name__ == '__main__':
     unittest.main()
-
